79c64096df6a85b64b1fb974a6b3c6e1c629c7a9
Marwan <marwan@Marwans-MacBook-Pro.local>
2022-05-10 12:44:14 +1000

    Rename jks file to ensure consistency

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ set -e
 set tw=0
 
 bash -x createConfig.sh policyproject kakfa-policy config/eventstreamspolicyproject.zip
-bash -x createConfig.sh truststore es-jks config/es-cert-ace.jks
+bash -x createConfig.sh truststore kafka.jks config/kafka.jks
 bash -x createConfig.sh setdbparms kafka-parms config/eventstreams-setdbparms.txt
 
 oc start-build afp-ace-produce 
